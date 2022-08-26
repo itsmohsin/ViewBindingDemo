cd360e399fdfd2282e950878c81cb517af43434d
Mohsin Khan <mohsinsmannat@gmail.com>
2022-08-26 19:57:59 +0530

    Added ViewBinding to App

--- app/src/main/java/com/itsmohsin/viewbindingdemo/MainActivity.kt
+++ app/src/main/java/com/itsmohsin/viewbindingdemo/MainActivity.kt
@@ -5,20 +5,22 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.itsmohsin.viewbindingdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val messageView = findViewById<TextView>(R.id.tvMessage)
-        val inputText = findViewById<EditText>(R.id.etName)
-        val submitButton = findViewById<Button>(R.id.btnSubmit)
-
-        submitButton.setOnClickListener {
-            val name = inputText.text.toString()
-            messageView.text = "Hello! $name"
-            inputText.setText("")
+        binding.apply {
+            btnSubmit.setOnClickListener {
+                val name = etName.text.toString()
+                tvMessage.text = "Hello! $name"
+                etName.setText("")
+            }
         }
     }
 }
