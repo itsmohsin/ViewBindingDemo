befc77a13e1c71ccdb78f02d6795f909ae70a636
itsmohsin <mohsinsmannat@gmail.com>
2022-08-26 19:16:04 +0530

    Added FrontEnd

--- app/src/main/java/com/itsmohsin/viewbindingdemo/MainActivity.kt
+++ app/src/main/java/com/itsmohsin/viewbindingdemo/MainActivity.kt
@@ -2,10 +2,23 @@ package com.itsmohsin.viewbindingdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val messageView = findViewById<TextView>(R.id.tvMessage)
+        val inputText = findViewById<EditText>(R.id.etName)
+        val submitButton = findViewById<Button>(R.id.btnSubmit)
+
+        submitButton.setOnClickListener {
+            val name = inputText.text.toString()
+            messageView.text = "Hello! $name"
+            inputText.setText("")
+        }
     }
 }
